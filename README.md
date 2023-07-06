81c57836a8c86fc27c518563b826f0303209c762
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2023-07-06 11:14:27 +0400

    Add tasks

---
+++ README.md
@@ -0,0 +1,14 @@
+# Homework 4-1
+
+დავალებები მოცემულია ფაილებში!
+
+## ამოცანების ამოსახსნელად უნდა იცოდეთ
+
+- loops
+- array
+- string
+- array's push method
+- array's and string's length property
+- prompt
+- comparison operators
+- functions
---
+++ task_4.js
@@ -0,0 +1,16 @@
+/**
+ * მოცემულია ორი "array", productNames და productPrices.
+ * productNames -ში ჩაყრილია "string" -ები.
+ * prdoctuPrices -ში ჩაყრილია "number" -ები.
+ * ორივე "array"მ-ს ერთი და იგივე ზომა აქვთ!
+ * დაწერეთ ფუნქცია, რომელიც მიიღებს 2 პარამეტრს, (productName, productPrice)
+ * და დააბრუნებს HTML-ის კოდს "string" -ად შემდეგი სტრუქტურით:
+ * 
+ * <div>
+ *      <p>Product Name</p>
+ *      <p>Product Price$</p>
+ * </div>
+ * 
+ * თითოეული ელემენტისთვის დააგენერირეთ ეს HTML კოდი და დაწერეთ "console.log" -ში მთლიანი აგებული სტრინგი.
+ * ანუ ყველა პროდუქტის HTML -ი ერთ დიდ "string" -ში უნდა შეინახოთ!
+ */
